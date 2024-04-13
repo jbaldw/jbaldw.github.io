5e9dee44afa309ee134a1ccd71c1e1f03cb04dd6
Joshua Baldwin <baldwinjd2@appstate.edu>
2024-04-13 17:12:56 -0400

    Theo Birthday

--- javascript/theo.js
+++ javascript/theo.js
@@ -6,5 +6,4 @@ document.querySelector('.front').addEventListener('click', function () {
 });
 
 // Autoplay audio on page load
-document.querySelector('.audio').attributes.removeNamedItem('muted');
 document.querySelector('.audio').play();
