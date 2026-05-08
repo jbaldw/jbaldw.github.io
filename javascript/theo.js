eb1f83459dd6b60b5d197cb37bec62c1932a4726
Joshua Baldwin <baldwinjd2development@gmail.com>
2026-05-08 15:57:30 -0400

    Full refactor and redesign of site - JB

--- javascript/theo.js
+++
@@ -1,9 +0,0 @@
-// When card with front or back class is clicked it sets or unsets the active class
-
-document.querySelector('.front').addEventListener('click', function () {
-    document.querySelector('.flip-card-style').classList.toggle('active');
-    document.querySelector('.spongebob').play();
-});
-
-// Autoplay audio on page load
-document.querySelector('.audio').play();
